bb4eeac2fe18da1e3a25897b786163bce3af3d8a
JR Boyens <jboyens@fooninja.org>
2018-05-29 23:02:21 -0700

    Basic setup

--- selenium/info.rkt
+++ selenium/info.rkt
@@ -2,9 +2,10 @@
 
 (define collection "selenium")
 (define version "0.1")
-(define deps '())
-(define build-deps '())
+(define deps '("base"))
+(define build-deps '("rackunit-lib"))
 (define implies '())
 (define compile-omit-paths '())
 (define test-omit-paths '())
-
+(define pkg-desc "Selenium library for Racket")
+(define pkg-authors '(jboyens))
