c270e2ba9216d412acb2933b92ff9d67da74559b
JR Boyens <jboyens@fooninja.org>
2018-05-29 22:55:36 -0700

    Initial import

---
+++ selenium/info.rkt
@@ -0,0 +1,10 @@
+#lang info
+
+(define collection "selenium")
+(define version "0.1")
+(define deps '())
+(define build-deps '())
+(define implies '())
+(define compile-omit-paths '())
+(define test-omit-paths '())
+
