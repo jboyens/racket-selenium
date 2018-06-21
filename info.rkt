35d7b0cbac349410fd7eade59e19165ee28c78b9
JR Boyens <jboyens@fooninja.org>
2018-06-21 01:11:01 -0700

    Updated dependencies

--- info.rkt
+++ info.rkt
@@ -1,6 +1,7 @@
 #lang setup/infotab
 (define version "0.1")
 (define collection 'multi)
-(define deps '())
-(define build-deps '())
+(define deps '("base" "http" "mock" "net-cookies-lib" "uri-template"))
+(define build-deps '("mock-rackunit" "rackunit-lib"))
 (define test-omit-paths '())
+
